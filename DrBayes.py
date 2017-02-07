3dc1cfbd746bc8ead9383ff76a9d6f2aaf553a0c
Sam Kennerly <samkennerly@gmail.com>
2017-02-07 06:52:12 -0500

    rewrite DrBayes for Python3

--- DrBayes.py
+++ DrBayes.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-
 '''
 DrBayes is a simple example of a naive Bayes classifier.
 It attempts to guess a patient's illness by asking yes/no questions.
 To run this program, open a terminal and type: python DrBayes.py
-Made and tested using Python 2.7.5 and Anaconda 1.8.0 packages.
+Made and tested using Anaconda 4.2.0 (Python 3.5.2)
 '''
-
 import pandas as pd
 import json
-
 
 
 ''' Get data from files '''
@@ -25,7 +22,6 @@ with open('Doctors.json') as doctor_file:
 	Doctors = json.load(doctor_file)
 
 
-
 ''' Diagnose the patient and recommend a doctor '''
 
 # At first, Dr. Bayes believes it's probably merely a flesh wound.
@@ -35,18 +31,18 @@ Prob = pd.Series(normalizer,index=all_illnesses)
 Prob.loc['Merely A Flesh Wound'] = 0.8
 
 # Query the patient and update the doctor's beliefs accordingly
-print "My name is Dr. Bayes, and I'm going to ask you a bunch of questions."
-print "Please enter Y for yes or N for no.\n"
+print( "My name is Dr. Bayes, and I'm going to ask you a bunch of questions." )
+print( "Please enter Y for yes or N for no.\n" )
 for symptom in all_symptoms:
 	
 	# If no answer, then skip to next symptom.
-	yn = raw_input("Are you experiencing unusual %s? " % symptom)
+	yn = input("Are you experiencing unusual %s? " % symptom)
 	if yn.upper() == 'Y':
 		has_symptom = True
 	elif yn.upper() == 'N':
 		has_symptom = False
 	else:
-		print "I'm sorry, I didn't understand your answer."
+		print( "I'm sorry, I didn't understand your answer." )
 		continue
 		
 	# Update the doctor's beliefs
@@ -62,21 +58,24 @@ for symptom in all_symptoms:
 		
 		
 # Print the most probable illnesses
-print "\nI believe these illnesses are consistent with your symptoms:"
-Prob.sort(ascending=False)
+print( "\nI believe these illnesses are consistent with your symptoms:" )
+Prob = Prob.sort_values(ascending=False)
 for illness in Prob.index:
 	pct = 100 * Prob.loc[illness].round(2)
 	if pct > 10:
-		print "%02d %% \t %s" % (pct,illness)
+		print( "%02d %% \t %s" % (pct,illness) )
 		
+
+''' Recommend a doctor '''
+
 # Recommend doctors who specialize in the most probable illness.
-# (This is a "reverse dictionary lookup.")
+# (This is a crude "reverse dictionary lookup.")
 diagnosis = Prob.idxmax()
-print "\nThese doctors specialize in %s:" % diagnosis
-for name, specialties in Doctors.iteritems():
+print( "\nThese doctors specialize in %s:" % diagnosis )
+for name, specialties in Doctors.items():
 	if diagnosis in specialties:
-		print "Dr.", name
-print
+		print( "Dr. " + name )
+print( )
 
 
 
